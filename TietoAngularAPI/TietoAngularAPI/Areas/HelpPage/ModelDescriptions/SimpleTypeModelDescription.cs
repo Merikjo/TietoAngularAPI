921e9ca677e67f90f5853a6a5ac82570c10e30c9
Johanna Merikanto <johanna.merikanto@stud.pointcollege.fi>
2017-10-27 19:20:43 +0300

    Luotu uusi projekti, jossa käytetään Angular API mallia yhdessä MVC mallin kanssa.

---
+++ TietoAngularAPI/TietoAngularAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TietoAngularAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TietoAngularAPI/TietoAngularAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TietoAngularAPI.WebApiApplication" Language="C#" %>
